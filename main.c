8e880e44fef2adc9227d42ccaee4d88b5811f39e
spsergey <skysurfer@SP.(none)>
2013-05-21 00:06:48 +0400

    Added usage of arguments. Better implementation of conditions (using case).

--- main.c
+++ main.c
@@ -1,46 +1,63 @@
 #include <iostream>
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
-  int blanks, tabs, newlines;
-  int c;
-  bool done = FALSE;
-  int lastchar = 0;
-  int symb = 0;
+int main(int argc, char ** argv)
+{
+    int blanks, tabs, newlines;
+    int c;
+    bool done = FALSE;
+    int lastchar = 0;
+    int i = 0;
+    char ch;
 
-  blanks = 0;
-  tabs = 0;
-  newlines = 0;
+    blanks = 0;
+    tabs = 0;
+    newlines = 0;
 
-  while(done == FALSE)
-  {
-    c = getchar();
+    ch = getopt (argc, argv, "ns");
 
-    if(c == ' ')
-      ++blanks;
-        else
-
-    if(c == '\t')
-      ++tabs;
-        else
-
-    if(c == '\n')
-      ++newlines;
-        else
-
-
-    if(c == EOF)
+    while (done == FALSE)
     {
-      if(lastchar != '\n')
-        ++newlines;
-      done = TRUE;
-    }
-    else
-        symb++;
-    lastchar = c;
-  }
-  int Total = blanks + tabs + newlines + i;
-  printf("Blanks: %d\nTabs: %d\nLines: %d\nSymbols: %d\nTotal: %d\n", blanks, tabs, newlines, symb, Total);
-  return 0;
-}
+        switch (c = getchar())
+        {
+        case ' ':
+            ++blanks;
+            break;
 
+        case '\t':
+            ++tabs;
+            break;
+
+        case '\n':
+            ++newlines;
+            break;
+
+        case EOF:
+        {
+            if(lastchar != '\n')
+                ++newlines;
+            done = TRUE;
+        }
+            break;
+
+        default:
+            i++;
+        }
+        lastchar = c;
+    }
+    if (ch == -1)
+        printf("Total number of symbols: %d\n", blanks + tabs + newlines + i);
+    switch (ch) {
+    case 'n':
+        printf ("Number of symbols without 'Newline' symbol: %d\n", blanks + tabs + i);
+        break;
+    case 's':
+        printf ("Number of symbols without spaces and 'Tab' symbol: %d\n", newlines + i);
+        break;
+    case '?':
+    default:
+        printf ("Usage: %s [-n] [-s].\n", argv[0]);
+    }
+    return 0;
+}
