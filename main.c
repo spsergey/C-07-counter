36c06a2be5ce0957ae1766308f994e365a6db876
spsergey <skysurfer@SP.(none)>
2013-05-21 02:46:53 +0400

    Remade the whole application. Made separate file for main function Counter. Made header file. Minor bug fix and optimization

--- main.c
+++ main.c
@@ -1,74 +1,48 @@
-#include <iostream>
-#include <stdio.h>
-#include <unistd.h>
-#include <getopt.h>
+#include <counter.h>
 
 int main(int argc, char **argv)
 {
-    int blanks, tabs, newlines;
-    int c;
-    bool done = false;
-    int lastchar = 0;
-    int i = 0;
+    int option_index = 0;
     int ch;
-
-    blanks = 0;
-    tabs = 0;
-    newlines = 0;
+    bool done = false;
+    int count [3];
 
     static struct option long_options[] =
     {
+        {"help",     no_argument,       0, 'h'},
         {"no-newline",     no_argument,       0, 'n'},
         {"no-spaces",     no_argument,       0, 's'},
         {0, 0, 0, 0}
     };
 
-    int option_index = 0;
-
-    while (done == false)
+    while ((ch = getopt_long (argc, argv, "nsh", long_options, &option_index)) != -1)
     {
-        switch (c = getchar())
+        if (ch == 'h')
         {
-        case ' ':
-            ++blanks;
-            break;
-
-        case '\t':
-            ++tabs;
-            break;
-
-        case '\n':
-            ++newlines;
-            break;
-
-        case EOF:
+            printf ("Use [-n or --no-newline] to count symbols without 'Newline' symbol\nUse [-s or --no-spaces] to count symbols without spaces and 'Tab' symbols\n");
+            return 0;
+        }
+        if (done == false)
         {
-            if(lastchar != '\n')
-                ++newlines;
+            counter(count);
             done = true;
         }
-            break;
-
-        default:
-            i++;
-        }
-        lastchar = c;
-    }
-    while ((ch = getopt_long (argc, argv, "ns", long_options, &option_index)) != -1)
-    {
-        if (ch == -1)
-            printf("Total number of symbols: %d\n", blanks + tabs + newlines + i);
         switch (ch) {
         case 'n':
-            printf ("Number of symbols without 'Newline' symbol: %d\n", blanks + tabs + i);
+            printf ("Number of symbols without 'Newline' symbol: %d\n", count[1]);
             break;
         case 's':
-            printf ("Number of symbols without spaces and 'Tab' symbol: %d\n", newlines + i);
+            printf ("Number of symbols without spaces and 'Tab' symbols: %d\n", count[2]);
             break;
         case '?':
         default:
             printf ("Usage: %s [-n or --no-newline] [-s or --no-spaces]\n", argv[0]);
         }
+    }
+    if (done == false)
+    {
+        counter(count);
+        printf("Total number of symbols: %d\n", count[0]);
     }
     return 0;
 }
