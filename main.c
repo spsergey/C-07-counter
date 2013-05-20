f5b8841be5a7c6b4266572315d713bef24618dc9
spsergey <skysurfer@SP.(none)>
2013-05-21 01:11:05 +0400

    Imroved usage of arguments. Now it supports long arguments.

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     int blanks, tabs, newlines;
     int c;
-    bool done = FALSE;
+    bool done = false;
     int lastchar = 0;
     int i = 0;
-    char ch;
+    int ch;
 
     blanks = 0;
     tabs = 0;
     newlines = 0;
 
-    ch = getopt (argc, argv, "ns");
+    static struct option long_options[] =
+    {
+        {"no-newline",     no_argument,       0, 'n'},
+        {"no-spaces",     no_argument,       0, 's'},
+        {0, 0, 0, 0}
+    };
 
-    while (done == FALSE)
+    int option_index = 0;
+
+    while (done == false)
     {
         switch (c = getchar())
         {
@@ -37,7 +45,7 @@ int main(int argc, char ** argv)
         {
             if(lastchar != '\n')
                 ++newlines;
-            done = TRUE;
+            done = true;
         }
             break;
 
@@ -46,18 +54,22 @@ int main(int argc, char ** argv)
         }
         lastchar = c;
     }
-    if (ch == -1)
-        printf("Total number of symbols: %d\n", blanks + tabs + newlines + i);
-    switch (ch) {
-    case 'n':
-        printf ("Number of symbols without 'Newline' symbol: %d\n", blanks + tabs + i);
-        break;
-    case 's':
-        printf ("Number of symbols without spaces and 'Tab' symbol: %d\n", newlines + i);
-        break;
-    case '?':
-    default:
-        printf ("Usage: %s [-n] [-s].\n", argv[0]);
+    while ((ch = getopt_long (argc, argv, "ns", long_options, &option_index)) != -1)
+    {
+        if (ch == -1)
+            printf("Total number of symbols: %d\n", blanks + tabs + newlines + i);
+        switch (ch) {
+        case 'n':
+            printf ("Number of symbols without 'Newline' symbol: %d\n", blanks + tabs + i);
+            break;
+        case 's':
+            printf ("Number of symbols without spaces and 'Tab' symbol: %d\n", newlines + i);
+            break;
+        case '?':
+        default:
+            printf ("Usage: %s [-n or --no-newline] [-s or --no-spaces]\n", argv[0]);
+        }
     }
     return 0;
 }
+
