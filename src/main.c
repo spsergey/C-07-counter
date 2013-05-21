72028408cbf141e3fad78ebb5c69b6fa1722133d
spsergey <student@localhost.localdomain>
2013-05-21 11:10:15 +0400

    Making the MAKEFILE. Copy all source files into directory src.

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include <counter.h>
+
+int main(int argc, char **argv)
+{
+    int option_index = 0;
+    int ch;
+    bool done = false;
+    int count [3];
+
+    static struct option long_options[] =
+    {
+        {"help",     no_argument,       0, 'h'},
+        {"no-newline",     no_argument,       0, 'n'},
+        {"no-spaces",     no_argument,       0, 's'},
+        {0, 0, 0, 0}
+    };
+
+    while ((ch = getopt_long (argc, argv, "nsh", long_options, &option_index)) != -1)
+    {
+        if (ch == 'h')
+        {
+            printf ("Use [-n or --no-newline] to count symbols without 'Newline' symbol\nUse [-s or --no-spaces] to count symbols without spaces and 'Tab' symbols\n");
+            return 0;
+        }
+        if (done == false)
+        {
+            counter(count);
+            done = true;
+        }
+        switch (ch) {
+        case 'n':
+            printf ("Number of symbols without 'Newline' symbol: %d\n", count[1]);
+            break;
+        case 's':
+            printf ("Number of symbols without spaces and 'Tab' symbols: %d\n", count[2]);
+            break;
+        case '?':
+        default:
+            printf ("Usage: %s [-n or --no-newline] [-s or --no-spaces]\n", argv[0]);
+        }
+    }
+    if (done == false)
+    {
+        counter(count);
+        printf("Total number of symbols: %d\n", count[0]);
+    }
+    return 0;
+}
+
